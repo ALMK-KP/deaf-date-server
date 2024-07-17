cc194ad6d32a9ff9e66045a3031607e2a16a13e0
asenataluczak <asenata.luczak@gmail.com>
2024-07-17 17:44:48 +0200

    express-rate-limit

--- index.ts
+++ index.ts
@@ -4,11 +4,17 @@ import cors from "cors";
 import routes from "./routes/routes";
 import compression from "compression";
 import helmet from "helmet";
+import RateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(compression());
 app.use(helmet());
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  limit: 20,
+});
+app.use(limiter);
 
 app.use(cors());
 app.use(express.json());
