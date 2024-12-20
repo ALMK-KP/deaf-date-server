44f7345e2d9a6d8de945ee0edbfcbff633b89d5b
asenataluczak <asenata.luczak@gmail.com>
2024-12-20 18:38:31 +0100

    streaming - added currentTime

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -17,7 +17,7 @@ export interface TrackWithoutId {
   order: number;
 }
 
-export type Track = TrackWithoutId & {id: number}
+export type Track = TrackWithoutId & { id: number };
 
 export interface TrackEncoded {
   id: number;
@@ -35,5 +35,5 @@ export interface User {
 export interface PlayerState {
   isPlaying: boolean;
   selectedTrack: Track | null;
+  currentTime: number;
 }
-
